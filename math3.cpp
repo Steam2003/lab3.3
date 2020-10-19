7d08ce2661065774af1bd083a86f50756e3dd5a0
Ivan Patiuk <gendalf2003@gmail.com>
2020-10-19 14:36:35 +0300

    lab3.3_fixed

--- math3.cpp
+++ math3.cpp
@@ -9,23 +9,24 @@ using namespace std;
 int main()
 {
 	double x; // вхідний аргумент
-	double R; // вхідний параметр
+	double R, R2; // вхідний параметр
 	double y; // результат обчислення виразу
-	cout << "R = "; cin >> R;
-	cout << "x = "; cin >> x;
+	cout << "R  = "; cin >> R;
+	cout << "R2 = "; cin >> R2;
+	cout << "x  = "; cin >> x;
 
 	// розгалуження в повній формі
 	if (x <= -6) // 1 участок
-		y = R / 2.;
+		y = R2 / 2.;
 	else
-		if (-6 < x && x <= -2 * R) // 2 участок
-			y = -(2 * R * R + R * x) / (-4 * R + 12);
+		if (-6 < x && x <= -2 * R2) // 2 участок
+			y = -(2 * R2 * R2 + R2 * x) / (-4 * R2 + 12);
 		else
-			if (-2 * R < x && x <= 0) // 3,4 участок
-				y = sqrt(R * R - pow((x + R), 2));
+			if (-2 * R2 < x && x <= 0) // 3,4 участок
+				y = sqrt(R2 * R2 - pow((x + R2), 2));
 				else
 					if (0 < x && x <= R) // 5,6 участок
-						y = -sqrt(pow((R / 2), 2) - pow((x - R / 2), 2));
+						y = -sqrt(R*R - (x - R)*(x-R));
 						else
 							y = (R * R - R * x) / 2.; // 7 участок 
 	
